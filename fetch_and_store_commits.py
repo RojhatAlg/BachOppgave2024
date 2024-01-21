c022ba4601088659c404246b9b6cd2861a03f26c
RojhatAlg <roal005@student.kristiania.no>
2024-01-21 07:33:51 +0100

    Added the first test base of db integration

---
+++ fetch_and_store_commits.py
@@ -0,0 +1,38 @@
+# Test fetching and storing commits to database.
+
+from pydriller import Repository
+import mysql.connector
+
+
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'password',
+    'database': 'db_commits',
+}
+
+# Establish MySQL connection
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+
+# Repository URL
+repo_url = 'https://github.com/RojhatAlg/00-welcome-to-cloud9'
+
+for commit in Repository(repo_url).traverse_commits():
+    commit_hash = commit.hash
+    author_name = commit.author.name
+
+    insert_query = "INSERT INTO commits (hash, author_name) VALUES (%s, %s)"
+    values = (commit_hash, author_name)
+
+    try:
+        cursor.execute(insert_query, values)
+        conn.commit()
+        print(f"Commit {commit_hash} by {author_name} successfully inserted into the database.")
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+
+# Close MySQL connection
+cursor.close()
+conn.close()
--- github-commits.py
+++ github-commits.py
@@ -1,4 +1,4 @@
 from pydriller import Repository
 
-for commit in Repository('https://github.com/amazon-ion/ion-java').traverse_commits():
+for commit in Repository('https://github.com/RojhatAlg/00-welcome-to-cloud9').traverse_commits():
     print('Hash {}, author {}'.format(commit.hash, commit.author.name))
